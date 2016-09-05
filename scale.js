71ffd193806e913c21a13f4c8f36625d5b89b704
qq940390 <940390@qq.com>
2016-09-05 12:55:23 +0800

    更新事件绑定对象，只支持集体绑定

--- scale.js
+++ scale.js
@@ -12,59 +12,52 @@
     }
 
     function getPage(event, page) {
-        return support.touch ? event.changedTouches[0][page] : event[page];
+        return support.touch ? event.originalEvent.changedTouches[0][page] : event[page];
     }
 
-    var ImagePinch = function() {};
-    ImagePinch.prototype = {
+    ImagePinch = {
         // 给初始化数据
-        init: function(imgDom, param) {
+        init: function(param) {
             var self = this;
             self.params = param || {};
 
             self.scale = 1;
+            self.disable = false;
             self.isMoving = false;
             self.buffMove = 5; //缓冲系数
             self.buffScale = 1.5; //放大系数
-            self.finger = false; //触摸手指的状态 false：单手指 true：多手指
+            self.morefinger = false; //触摸手指的状态 false：单手指 true：多手指
             self.overTopSide = false;   //移动超出了上边界
             self.overBottomSide = false;//移动超出了下边界
             self.overLeftSide = false;  //移动超出了左边界
             self.overRightSide = false; //移动超出了右边界
             self.isResetting = false;
+            self.isTap = true;
             self.distX = 0;
             self.distY = 0;
             self.newX = 0;
             self.newY = 0;
+            self.handle = self.params.handle;   //css selector
 
-            var zoomMask = imgDom.parentNode;
-            if(self.params.verticalMoveCenter == true) {    //移动时总以垂直中心为准
-                imgDom.style.cssText = "position: absolute;top: 50%;margin-top:-" + (imgDom.offsetHeight / 2) + "px";
-            } else {    //以父层的真实高宽移动为准
-                imgDom.style.cssText = "position: absolute;";
-            }
-            self.imgBaseWidth = imgDom.offsetWidth;     //图片基础宽度
-            self.imgBaseHeight = imgDom.offsetHeight;   //图片基础高度
-            self.element = imgDom;
-            self.params.wrapWidth = zoomMask.offsetWidth;    //可视区域宽度
-            self.params.wrapHeight = zoomMask.offsetHeight;    //可视区域高度
-            self.params.realWidth = imgDom.width;             //地图宽度
-            self.params.realHeight = imgDom.height;            //地图高度
-            self.addEventStart();
-        },
-        addEventStart: function() {
-            var self = this;
+            var zoomMask = $(self.handle);
+            $(self.handle).find('img').on('load', function() {
+                if(self.params.verticalMoveCenter == true) {    //移动时总以垂直中心为准
+                    this.style.cssText = "position: absolute;top: 50%;margin-top:-" + (this.offsetHeight / 2) + "px";
+                } else {    //以父层的真实高宽移动为准
+                    this.style.cssText = "position: absolute;";
+                }
+                self.imgBaseWidth = this.offsetWidth;     //图片基础宽度
+                self.imgBaseHeight = this.offsetHeight;   //图片基础高度
+                self.wrapWidth = zoomMask.width();    //可视区域宽度
+                self.wrapHeight = zoomMask.height();    //可视区域高度
+                self.realWidth = this.width;             //地图宽度
+                self.realHeight = this.height;            //地图高度
+                self.outDistY = (self.realHeight - self.wrapHeight) / 2; //图片超过一屏的时候有用
+                self.width = self.realWidth - self.wrapWidth; //地图的宽度减去可视区域的宽度
+                self.height = self.realHeight - self.wrapHeight; //地图的高度减去可视区域的高度
+            });
 
-            self.wrapWidth = self.params.wrapWidth || 0; //可视区域宽度
-            self.wrapHeight = self.params.wrapHeight || 0; //可视区域高度
-            self.realWidth = self.params.realWidth || 0; //地图宽度
-            self.realHeight = self.params.realHeight || 0; //地图高度
-
-            self.outDistY = (self.realHeight - self.wrapHeight) / 2; //图片超过一屏的时候有用
-
-            self.width = self.realWidth - self.wrapWidth; //地图的宽度减去可视区域的宽度
-            self.height = self.realHeight - self.wrapHeight; //地图的高度减去可视区域的高度
-
+            //绑定事件
             var myTouchStartFun = function(e) {
                 self._touchstart(e);
             }
@@ -80,63 +73,82 @@
             }
             self.touchEndFun = myTouchEndFun;
 
-            self.element.addEventListener("touchstart", myTouchStartFun, false);
-            self.element.addEventListener("touchmove", myTouchMoveFun, false);
-            self.element.addEventListener("touchend", myTouchEndFun, false);
+            $(self.handle).on("touchstart", myTouchStartFun);
+            $(self.handle).on("touchmove", myTouchMoveFun);
+            $(self.handle).on("touchend", myTouchEndFun);
         },
         // 重置坐标数据
         destroy: function() {
-            this.element.removeEventListener('touchstart', this.touchStartFun);
-            this.element.removeEventListener('touchmove', this.touchMoveFun);
-            this.element.removeEventListener('touchend', this.touchEndFun);
+            $(this.handle).off('touchstart', this.touchStartFun);
+            $(this.handle).off('touchmove', this.touchMoveFun);
+            $(this.handle).off('touchend' ,this.touchEndFun);
         },
         // 更新地图信息
-        _changeData: function() {
+        _resetData: function(e) {
+            if(e) {
+                this.element = $(e.target).parent().find('img').get(0);
+            }
+
             this.realWidth = this.element.offsetWidth; //地图宽度
             this.realHeight = this.element.offsetHeight; //地图高度
-            //this.outDistY = (this.realHeight - this.wrapHeight)/2; //当图片高度超过屏幕的高度时候。图片是垂直居中的，这时移动有个高度做为缓冲带
             this.width = this.realWidth - this.wrapWidth; //地图的宽度减去可视区域的宽度
             this.height = this.realHeight - this.wrapHeight; //地图的高度减去可视区域的高度
         },
         _touchstart: function(e) {
             var self = this;
+            if(self.disable == true) return;
+            self.isTap = true;
 
-            self.tapDefault = false;
+            var touchTarget = e.originalEvent.targetTouches.length; //获得触控点数
+            // 获取开始坐标
+            self.basePageX = getPage(e, "pageX");
+            self.basePageY = getPage(e, "pageY");
 
-            var touchTarget = e.targetTouches.length; //获得触控点数
+            self._resetData(e); //重新初始化图片、可视区域数据，由于放大会产生新的计算
 
-            self._changeData(); //重新初始化图片、可视区域数据，由于放大会产生新的计算
-
-            if (touchTarget == 1) {
-                // 获取开始坐标
-                self.basePageX = getPage(e, "pageX");
-                self.basePageY = getPage(e, "pageY");
-
-                self.finger = false;
+            if(self.scale == 1) {
+                //原比例时只允许缩放
+                if (touchTarget > 1) {
+                    // 禁止默认事件
+                    self.eventStop(e);
+                    self.morefinger = true;
+                    var _dist = self.getTouchDist(e);
+                    self.startFingerDist = _dist.dist;
+                    self.startFingerX = _dist.x;
+                    self.startFingerY = _dist.y;
+                } else {
+                    self.morefinger = false;
+                }
             } else {
-                // 禁止默认事件
-                self.eventStop(e);
-
-                self.finger = true;
-
-                self.startFingerDist = self.getTouchDist(e).dist;
-                self.startFingerX = self.getTouchDist(e).x;
-                self.startFingerY = self.getTouchDist(e).y;
+                //放大后允许移动和缩放
+                if (touchTarget == 1) {
+                    self.morefinger = false;
+                } else {
+                    // 禁止默认事件
+                    self.eventStop(e);
+                    self.morefinger = true;
+                    var _dist = self.getTouchDist(e);
+                    self.startFingerDist = _dist.dist;
+                    self.startFingerX = _dist.x;
+                    self.startFingerY = _dist.y;
+                }
             }
         },
         _touchmove: function(e) {
             var self = this;
-            self.tapDefault = true;
+            if(self.disable == true) return;
+            self.isTap = false; //滑动后不触发tap事件
 
-            var touchTarget = e.targetTouches.length; //获得触控点数
+            var touchTarget = e.originalEvent.targetTouches.length; //获得触控点数
 
-            if (touchTarget == 1 && !self.finger) {
+            if (touchTarget == 1 && !self.morefinger) {
                 self._move(e);
             }
 
-            if(!self.overLeftSide && !self.overRightSide && !self.overTopSide && !self.overBottomSide) {
+            if(touchTarget == 1 && (!self.overLeftSide && !self.overRightSide && !self.overTopSide && !self.overBottomSide)) {
                 // 禁止默认事件
                 self.eventStop(e);
+                return false;
             }
 
             if (touchTarget >= 2) {
@@ -145,24 +157,10 @@
         },
         _touchend: function(e) {
             var self = this;
-            console.log(self.tapDefault)
+            if(self.disable == true) return;
 
-            if (!self.finger && !self.tapDefault && self.scale > 1) {
-                // 禁止默认事件
-                self.eventStop(e);
-                self.reset();
-                return;
-            } else if(!self.finger && !self.tapDefault && self.scale == 1) {
-                if(typeof self.params.onTap == 'function') {
-                    self.params.onTap(e);
-                }
-                // 禁止默认事件
-                self.eventStop(e);
-                return false;
-            }
-
-            self._changeData(); //重新计算数据
-            if (self.finger) {
+            self._resetData(e); //重新计算数据
+            if (self.morefinger) {
                 self.distX = -self.imgNewX;
                 self.distY = -self.imgNewY;
             }
@@ -175,8 +173,20 @@
             } else if (self.distX < -self.width) {
                 self.newX = -self.width;
             }
-            self.setImagePos();
+            self.setImagePos(e);
             self.isMoving = false;
+
+            if (!self.morefinger && self.isTap && self.scale > 1) {
+                //被放大后，点击后复原
+                self.eventStop(e);
+                self.reset();
+                return false;
+            } else if(!self.morefinger && self.isTap && self.scale == 1) {
+                if(typeof self.params.onTap == 'function') {
+                    self.params.onTap(e);
+                    return false;
+                }
+            }
         },
         _move: function(e) {
 
@@ -184,6 +194,8 @@
                 pageX = getPage(e, "pageX"), //获取移动坐标
                 pageY = getPage(e, "pageY");
             self.isMoving = true;
+
+            self.element = $(e.target).parent().find('img').get(0);
 
             // 获得移动距离
             self.distX = (pageX - self.basePageX) + self.newX;
@@ -250,12 +262,14 @@
             }
 
             self.refresh(self.moveX, self.moveY, false, false, "0s", "ease");
-            self.finger = false;
+            self.morefinger = false;
         },
         // 图片缩放
         _zoom: function(e) {
             var self = this;
             self.eventStop(e);
+
+            self.element = $(e.target).parent().find('img').get(0);
 
             var nowFingerDist = self.getTouchDist(e).dist, //获得当前长度
                 ratio = nowFingerDist / self.startFingerDist, //计算缩放比
@@ -270,7 +284,7 @@
             if (imgWidth >= self.imgBaseWidth) {
                 self.element.style.width = imgWidth + "px";
                 self.refresh(-self.imgNewX, -self.imgNewY, false, false, "0s", "ease");
-                self.finger = true;
+                self.morefinger = true;
                 newImgWidth = imgWidth;
             } else {
                 if (imgWidth < self.imgBaseWidth) {
@@ -281,12 +295,14 @@
 
             self.scale = parseInt(newImgWidth) / self.imgBaseWidth;
             self.scale.toFixed(4);
-            self.finger = true;
+            self.morefinger = true;
         },
         // 重置数据
-        setImagePos: function() {
+        setImagePos: function(e) {
             var self = this,
                 hideTime = ".2s";
+            self.element = $(e.target).parent().find('img').get(0);
+
             if(self.params.verticalMoveCenter == true) {    //移动时总以垂直中心为准
                 if (self.height < 0) {  //还没有超过一屏时
                     self.newY = -Math.round(self.element.offsetHeight - self.imgBaseHeight) / 2;
@@ -347,7 +363,7 @@
         reset : function() {
             this.isResetting = true;
             this.refresh(0, 0, this.imgBaseWidth, false, "0s", "ease");
-            this._changeData();
+            this._resetData();
             this.distX = 0;
             this.distY = 0;
             this.newX = 0;
@@ -367,10 +383,10 @@
                 y3 = 0,
                 result = {};
 
-            x1 = e.touches[0].pageX;
-            x2 = e.touches[1].pageX;
-            y1 = e.touches[0].pageY - document.body.scrollTop;
-            y2 = e.touches[1].pageY - document.body.scrollTop;
+            x1 = e.originalEvent.touches[0].pageX;
+            x2 = e.originalEvent.touches[1].pageX;
+            y1 = e.originalEvent.touches[0].pageY - document.body.scrollTop;
+            y2 = e.originalEvent.touches[1].pageY - document.body.scrollTop;
 
             if (!x1 || !x2) return;
 
@@ -393,9 +409,9 @@
             return result;
         },
         eventStop: function(e) {
-            e.preventDefault();
-            e.stopPropagation();
             try {
+                e.preventDefault();
+                e.stopPropagation();
                 e.originalEvent.preventDefault();
                 e.originalEvent.stopPropagation();
             } catch(e) {}
